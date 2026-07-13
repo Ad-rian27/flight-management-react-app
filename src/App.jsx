a6dd6cd8e55e28baa7130dac09fb0f8407e70e28
ShaunPeter04 <shaunpeter04@gmail.com>
2026-07-13 13:17:14 +0530

    Link tag added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import NavigationBar from './components/NavigationBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavigationBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NavigationBar />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
     return (
         <div>
 
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm py-3">
-                <div class="container">
-                    <a class="navbar-brand fw-bold fs-4 d-flex align-items-center" href="/">
-                        <span class="text-primary me-2">✈</span> Flight Management
-                    </a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm py-3">
+                <div className="container">
+                    <Link className="navbar-brand fw-bold fs-4 d-flex align-items-center" to="/">
+                        <span className="text-primary me-2">✈</span> Flight Management
+                    </Link>
 
-                    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ms-auto gap-2 text-center text-lg-start pt-3 pt-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active px-3 rounded" aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto gap-2 text-center text-lg-start pt-3 pt-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active px-3 rounded" aria-current="page" to="/">Home</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link px-3 rounded" href="/add">Add Flight</a>
+                                <Link className="nav-link px-3 rounded" to="/add">Add Flight</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link px-3 rounded" href="/view">View Flights</a>
+                                <Link className="nav-link px-3 rounded" to="/view">View Flights</Link>
                             </li>
                         </ul>
                     </div>
