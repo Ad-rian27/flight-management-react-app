8a22bd6f8b7c9f30eba67ccafea3c119a93a445e
Ad-rian27 <rian270627@gmail.com>
2026-07-13 13:14:12 +0530

    Minor changes

--- src/components/AddFlight.jsx
+++ src/components/AddFlight.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import NavigationBar from "./NavigationBar";
 
 const AddFlight = () => {
 
@@ -64,6 +65,7 @@ const AddFlight = () => {
     return (
         <div>
 
+<NavigationBar />
             <div className="container mt-5">
 
                 <div className="row">
